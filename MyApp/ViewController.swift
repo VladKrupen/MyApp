04ac6901bfb3ac57dde9dfb964e0e1ab66344ef7
Vlad Krupen <air7360@gmail.com>
2024-04-21 16:52:11 +0300

    Initial commit

---
+++ MyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyApp
+//
+//  Created by Vlad on 21.04.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+
+}
+
